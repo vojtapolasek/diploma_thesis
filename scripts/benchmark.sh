8177947183c87f49a376585c5434df9f91ab0c47
Polášek Vojtěch <Vojtech.Polasek@gmail.com>
2019-04-11 13:52:44 +0200

    replaced compile.sh and benchmark.sh with latest versions

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-
-
-gcc -o benchmark benchmark.c -lcryptsetup
-for time in 1000 2000 3000 4000 5000
+for time in 1000 2000 3000 4000 5000 10000 20000
 do mkdir $time
-for threads in `seq 1 8`
+for threads in 1 2 3 4
 do echo time $time threads $threads
-./benchmark -t $time -p $threads -r 1 -c > $time/stdmem$threads.csv
+./benchmark -t $time -p $threads -r 100 -c -m 3145728 > $time/stdmem$threads.csv
 done
 done
--- scripts/compile.sh
+++ scripts/compile.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+
+rm -rf cryptsetup-2.1.0/
+tar xf cryptsetup-2.1.0.tar.xz
+if [ "$1" = "-b" ]; then
+sudo apt-get update
+sudo apt-get install libssl-dev libpopt-dev uuid-dev libdevmapper-dev libjson-c-dev
 cd cryptsetup-2.1.0/
 make clean
-./configure --disable-udev --disable-blkid CPPFLAGS='-I../usr/include/ -O3 -march=x86-64' LDFLAGS='-L../usr/lib/x86_64-linux-gnu/'
+patch lib/utils.c ../utils.c.patch
+./configure --disable-udev --disable-blkid
+read -n1 -r -p "waiting"
 make
 cd ..
-if [ "$1" = "-b" ]; then
-cd cpulimit
-make clean
-make
-cd ..
-gcc -O3 -march=x86-64 -Wl,-Lcryptsetup-2.1.0/.libs/ -o benchmark -I cryptsetup-2.1.0/lib/ -Wl,-rpath=cryptsetup-2.1.0/.libs/ benchmark.c -lcryptsetup -pthread
+gcc -O3 -march=native -Wl,-Lcryptsetup-2.1.0/.libs/ -o benchmark -I cryptsetup-2.1.0/lib/ -Wl,-rpath=cryptsetup-2.1.0/.libs/ benchmark.c -lcryptsetup
 fi
 if [ "$1" = "-c" ]; then
+cd cryptsetup-2.1.0/
+make clean
+./configure --disable-udev --disable-blkid CPPFLAGS='-I../usr/include/ -march=x86-64' LDFLAGS='-L../usr/lib/x86_64-linux-gnu/'
+make
+cd ..
 gcc -O3 -march=x86-64 -Wl,-Lcryptsetup-2.1.0/.libs/ -o cracker -I cryptsetup-2.1.0/lib/ -Wl,-rpath=cryptsetup-2.1.0/.libs/ cracker.c -lcryptsetup
 gcc -O3 -march=x86-64 -Wl,-Lcryptsetup-2.1.0/.libs/ -o cracker_multithreaded -I cryptsetup-2.1.0/lib/ -Wl,-rpath=cryptsetup-2.1.0/.libs/ cracker_multithreaded.c -lcryptsetup -pthread
 fi
