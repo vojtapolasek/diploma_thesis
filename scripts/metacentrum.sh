725d2531b2f1cf6abce7879472a0eb3acba8b536
Polášek Vojtěch <Vojtech.Polasek@gmail.com>
2019-04-14 16:11:09 +0200

    updated scripts adding tables

--- scripts/metacentrum.sh
+++ scripts/metacentrum.sh
@@ -1,10 +1,52 @@
 #!/bin/bash
+OPTIND=1
+
 
 count=100
 lanes=4
 iterations=6
 memory=1048576
+outputdir="" #fill in
 
+while getopts "hc:M:T:L:o:" opt
+do case $opt in
+h)
+echo "-o for output directory, required"
+echo "-c for specifying number of computations, default" $count
+echo "-M for memory parameter, default" $memory
+echo "-L for number of lanes, default" $lanes
+echo "-I for number of iterations, default " $iterations
+exit 0
+;;
+o)
+outputdir=$OPTARG
+;;
+c)
+count=$OPTARG
+;;
+M)
+memory=$OPTARG
+;;
+L)
+lanes=$OPTARG
+;;
+I)
+iterations=$OPTARG
+;;
+?)
+echo "invalid argument supplied"
+exit 1
+;;
+esac
+done
+shift $((OPTIND-1))
+[ "${1:-}" = "--" ] && shift
+
+if [ ! -d "$outputdir" ] || [ ! -x "$outputdir" ]
+then
+echo "invalid or inaccessible output directory"
+exit 1
+fi
 
 cd $SCRATCHDIR
 source /storage/brno2/home/vojtapolasek/.brew_enable
@@ -16,5 +58,5 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release ..
 make
 for batch in 1 2 4 8 16 32 64 128 256 512 1024
-do ./argon2-gpu-bench -m cpu -L $lanes -T $iterations -M $memory -s $count -b $batch > /storage/brno2/home/vojtapolasek/results/$batch.log 2>&1
+do ./argon2-gpu-bench -m cpu -L $lanes -T $iterations -M $memory -s $count -b $batch > $outputdir/$batch.log 2>&1
 done
